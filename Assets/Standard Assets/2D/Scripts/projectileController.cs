af51651dc743312b73f1a80f831d3efd1d157d10
AdamO1997 <2196850o@student.gla.ac.uk>
2017-10-21 13:37:54 +0100

    added sprites

--- Assets/Standard Assets/2D/Scripts/projectileController.cs
+++ Assets/Standard Assets/2D/Scripts/projectileController.cs
@@ -6,10 +6,16 @@ public class projectileController : MonoBehaviour {
 
     public float speed;
     private Rigidbody2D rb;
+    public GameObject player;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        if(player.transform.localScale.x < 0)
+        {
+            speed = -speed;
+        }
 	}
 	
 	// Update is called once per frame
